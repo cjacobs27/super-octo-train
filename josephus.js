b46c0aa8fe700997a70adaf7d092b362c42bcea6
Chelsey <jacobs27c@gmail.com>
2018-01-22 11:38:27 +0000

    Script finished (no styling, run in dev console)

--- josephus.js
+++ josephus.js
@@ -1,20 +1,20 @@
-// solving Josephus Problem of any size
-// crowd = 2a + l
+// solving Josephus Problem of any crowd size under 1,048,576
+// crowd = 2^a + l
 // Winner(n) = 2l + 1
 
-// the selected prime is the "2a" part of "crowd = 2a + l"
-var primelist, primeShortlist, selectedPrime
-// primelist = []
+// the selected prime is the "2^a" part of "crowd = 2^a + l"
+var primeShortlist, selectedPrime
 primeShortlist = []
 
-//take crowd size (var crowd)
+//take crowd size
 var crowd = prompt("Please enter a crowd size (integer)")
 
 // finds the biggest prime that's <= to crowd size (finding 2a)
 function primeFinder() {
-  // generates list of all primes of 2 up to 2 ^20
+  // generates all primes of 2 up to 2 ^20
   for (i=1; i<20; i++) {
     var prime = Math.pow(2,i)
+    // saves primes under crowd size to shortlist
       if (prime <= crowd) {
         primeShortlist.push(Math.pow(2,i))
       }
@@ -25,7 +25,6 @@ function primeFinder() {
 }
 
 // now to find l
-
 function findL() {
   selectedPrime = primeFinder()
   // crowd = 2a + l
