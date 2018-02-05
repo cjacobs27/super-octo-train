55d8592fb21ae3566b0c216cb68dd628f061aa67
Chelsey <jacobs27c@gmail.com>
2018-02-05 10:21:48 +0000

    Add message display system
    keeps code more organised

--- dice game/dice_app.js
+++ dice game/dice_app.js
@@ -6,7 +6,7 @@ var gameCtrl = (function() {
 
 // var makeFirstRound = function(){
   var Round = function(roundCount,p1MoneyCounter,p2MoneyCounter,continues,potValue,gameEnd,
-  forOrAgainst,activePlayer,passivePlayer){
+  forOrAgainst,forOrAgainstChosen,activePlayer,passivePlayer,message){
     this.roundCount = 0;
     this.p1MoneyCounter = 100;
     this.p2MoneyCounter = 100;
@@ -14,16 +14,93 @@ var gameCtrl = (function() {
     this.potValue = 0;
     this.gameEnd = 0;
     this.forOrAgainst = 0;
+    this.forOrAgainstChosen = 0;
     this.activePlayer = 0;
     this.passivePlayer = 1;
+    // methods that use message get the query selection etc from uiCtrl
+    this.message = "m" + message;
 }
 
 return {
   makeFirstRound: function() {
-    var round = new Round(0,100,100,0,0,0,0,0,1);
+    var round = new Round(0,100,100,0,0,0,0,0,0,1,0);
     return round
+  },
+  // REMEMBER THE COMMA ABOVE! if no comma = unexpected identifier
+  submitBet: function() {
+    console.log("Submit bet button worked.");
+    // var DOMstrings = uiCtrl.getDOMstrings();
+    //   shooterBet = Number(document.querySelector(DOMstrings.leftBetAmount).value)
+    //   // makes sure passive player is set to correct person
+    //   // activePlayer === 0 ? passivePlayer = 1 : passivePlayer = 0;
+    //   betterBet = Number(document.querySelector(DOMstrings.rightBetAmount).value)
+    //   if (betterBet == "0" && forOrAgainstChosen == 0) {
+    //     document.querySelector('#notification').innerHTML =
+    //     "You need to type in your bet amount.";
+    //   }
+    //   else if (bettingTime == 1) {
+    //           if (betterBet <= betterMoneyCounter && betterBet == shooterBet) {
+    //             potValue = Number(potValue) + Number(betterBet);
+    //             document.querySelector("#pot").textContent = "£" + potValue
+    //             document.querySelector('#MoneyCounter-' + passivePlayer).textContent = "£" + (betterMoneyCounter - betterBet)
+    //             betterMoneyCounter = (betterMoneyCounter - betterBet)
+    //             document.querySelector('.btn-roll').style.display = 'initial';
+    //             document.querySelector('#notification').innerHTML =
+    //             "Ok, time to roll the dice!";
+    //             bettingTime = 0
+    //           }
+    //           else if (Number(betterBet) !== Number(shooterBet) && forOrAgainst == 0) {
+    //             document.querySelector('#notification').innerHTML =
+    //             "Your bet needs to match Player 1's bet, since you picked For. I'd do it automatically but I"
+    //             + " don't know any jQuery yet :(";
+    //           }
+    //           else if (Number(betterBet) !== Number(shooterBet) && forOrAgainst == 1) {
+    //             potValue = Number(potValue) + Number(betterBet);
+    //             document.querySelector("#pot").textContent = "£" + potValue
+    //             document.getElementById('MoneyCounter' + passivePlayer).textContent = "£" + (betterMoneyCounter - betterBet)
+    //             betterMoneyCounter = (betterMoneyCounter - betterBet)
+    //             document.querySelector('.btn-roll').style.display = 'initial';
+    //             document.querySelector('#notification').innerHTML =
+    //             "Ok, time to roll again.";
+    //             bettingTime = 0
+    //           }
+    //           else if (betterBet > betterMoneyCounter){
+    //             document.querySelector('#notification').innerHTML =
+    //             "You can't bet more money than you have!";
+    //           }
+    //           else if (bettingTime == 0) {
+    //             document.querySelector('#notification').innerHTML =
+    //             "You can't bet right now.";
+    //           }
+    //           else if (betterBet <= 0) {
+    //             document.querySelector('#notification').innerHTML =
+    //             "You can't bet negative money.";
+    //           }
+    //           else if (bettingTime == 1 && forOrAgainstChosen == 1) {
+    //             potValue = Number(potValue) + Number(shooterBet);
+    //             document.querySelector("#pot").textContent = "£" + potValue
+    //             document.getElementById('#MoneyCounter-' + activePlayer).textContent = "£" + (betterMoneyCounter - betterBet)
+    //             betterMoneyCounter = (betterMoneyCounter - betterBet)
+    //             document.querySelector('#notification').innerHTML =
+    //             "Player 1 doesn't have to match that bet if they don't want to.";
+    //             //bettingTime = 0
+    //           }
+    //         else {
+    //           bettingTime = 0
+    //           document.querySelector('#notification').innerHTML =
+    //           "Ok, time to roll the dice.";
+    //           document.querySelector('.btn-roll').style.display = 'initial';
+    //         }}
+    //   else if (betterBet == "0" && forOrAgainstChosen == 1 && forOrAgainst == 1) {
+    //     document.querySelector('#notification').innerHTML =
+    //     "The better chose not to bet.";
+    //   }
+    //   else {
+    //     //nothing
+    //   }
+    }
   }
-  }
+  // }
 
 })();
 
@@ -42,13 +119,25 @@ var uiCtrl = (function() {
     continueButton: '.btn-continue',
     forAgainstSelector: '#for-against',
     newGameButton: '.btn-new',
-    // leftBetButton: '#btn-bet0',
+    leftBetAmount:'#bet-val-0',
+    rightBetAmount: '#bet-val-1',
+    leftBetButton: '#btn-bet0',
     rightBetButton: '#btn-bet1'
+  }
+
+  var gameMessages = {
+    m0: document.querySelector(DOMstrings.notificationBox).innerHTML =
+    "Please flip a coin to decide who's Player 1 (who rolls the dice in the first round), then ante up",
+    m1: document.querySelector(DOMstrings.notificationBox).innerHTML =
+    "You need to type in your bet amount."
   }
 
 return {
   getDOMstrings: function() {
       return DOMstrings;
+  },
+  getGameMessages: function() {
+    return gameMessages;
   }
 }
 
@@ -60,13 +149,17 @@ var controller = (function(gameCtrl, uiCtrl) {
 
       var resetGame = function(){
         var DOMstrings = uiCtrl.getDOMstrings();
+        var gameMessages = uiCtrl.getGameMessages();
         var round = gameCtrl.makeFirstRound();
 
+// a Round object is instantiated (as 'round') by makeFirstRound() below
+// as the game progresses the object is updated
         document.querySelector(DOMstrings.moneyCounter + round.activePlayer).textContent = "£" + round.p1MoneyCounter
         document.querySelector(DOMstrings.moneyCounter + round.passivePlayer).textContent = "£" + round.p2MoneyCounter
         document.querySelector(DOMstrings.pot).textContent = "£" + round.potValue
-        document.querySelector(DOMstrings.notificationBox).textContent =
-        "Please flip a coin to decide who's Player 1 (who rolls the dice in the first round), then ante up"
+// messages like these could go into a MAP once the app is ready for ES6 (to keep them organised)
+        document.querySelector(DOMstrings.notificationBox).innerHTML = gameMessages[round.message]
+        // gameMessages.displayMessage(gameMessages[round.message])
         diceDOM1 = document.querySelector(DOMstrings.dice1)
         diceDOM2 = document.querySelector(DOMstrings.dice2)
         diceDOM1.src = 'dice-5.png';
@@ -74,6 +167,8 @@ var controller = (function(gameCtrl, uiCtrl) {
         document.querySelector(DOMstrings.rollButton).style.display = 'none';
         document.querySelector(DOMstrings.continueButton).style.display = 'none';
         document.querySelector(DOMstrings.forAgainstSelector).style.display = 'none';
+        // changing it to just the better bet button for now - might remove shooter bet button from HTML too
+        document.querySelector(DOMstrings.leftBetButton).style.display = 'none';
         // just in case .label got added last game
         document.querySelector(DOMstrings.notificationBox).classList.remove('label')
         console.log("New Game button clicked")
@@ -88,7 +183,7 @@ var controller = (function(gameCtrl, uiCtrl) {
       // document.querySelector(DOMstrings.continueButton).addEventListener('click', continueGame)
       document.querySelector(DOMstrings.newGameButton).addEventListener('click', controller.init)
       // document.querySelector(DOMstrings.leftBetButton).addEventListener('click', submitShooterBet)
-      // document.querySelector(DOMstrings.rightBetButton).addEventListener('click', submitBetterBet)
+      document.querySelector(DOMstrings.rightBetButton).addEventListener('click', gameCtrl.submitBet)
       console.log("Event listeners set up")
     };
 
@@ -97,7 +192,7 @@ var controller = (function(gameCtrl, uiCtrl) {
         console.log('Application has started.');
         resetGame();
         setupEventListeners();
-
+// future methods: continue, win condition router(?)
       }
 
 }
