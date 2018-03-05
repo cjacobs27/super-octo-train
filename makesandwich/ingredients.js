18155a06e0e2f2d1e67f8d525253cddb2ed564fc
Chelsey <jacobs27c@gmail.com>
2018-03-05 16:21:11 +0000

    All <li> colours randomised on mouseOver of any <li>

--- makesandwich/ingredients.js
+++ makesandwich/ingredients.js
@@ -1,30 +1,30 @@
 const Ingredients = React.createClass({
 
     setColours :   function(e) {
-            document.getElementById('ing').style.backgroundColor = '#'+Math.floor(Math.random() * 16777215).toString(16);
-        // return document.e.style.backgroundColor = '#'+Math.floor(Math.random() * 16777215).toString(16);
+            // var elementsList = document.getElementsByTagName('li');
+            var elementsList = document.getElementsByClassName('ingList');
+            for (let i = 0; i < elementsList.length; i++) {
+                elementsList[i].style.backgroundColor = '#'+Math.floor(Math.random() * 16777215).toString(16);
+            }
 
-    },
 
-    divColours : {
-        // generate a random hex colour - all <li> are same random colour
-        // backgroundColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-        //  backgroundColor: this.getColour,
-        border: "2px solid #000000",
-    },
+        // switch statement to set specific colours if value is specific thing ie condiment name?
+            },
 
     render: function() {
         var displayIngredient = (ingredient) =>
-        <div id ={"ing"} style={this.divColours} onMouseOver= {this.setColours} >
+        <div id ={"ing"} className={'ingList'} onMouseOver={this.setColours}>
             <li>
         {ingredient}
         </li>
         </div>;
 
         return (
+            <div>
         <ul>
             {this.props.items.map(displayIngredient)}
         </ul>
+            </div>
          );
     }
 
