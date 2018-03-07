31b7ddc27cfb0cc27cd8cf81e239b2db99ebe539
Chelsey <jacobs27c@gmail.com>
2018-03-07 13:59:17 +0000

    Initial changes to try and implement set colours for certain ings

--- makesandwich/ingredients.js
+++ makesandwich/ingredients.js
@@ -1,19 +1,19 @@
 const Ingredients = React.createClass({
 
     setColours :   function(e) {
-            // var elementsList = document.getElementsByTagName('li');
             var elementsList = document.getElementsByClassName('ingList');
             for (let i = 0; i < elementsList.length; i++) {
+                // randomising the unrecognised colours
                 elementsList[i].style.backgroundColor = '#'+Math.floor(Math.random() * 16777215).toString(16);
+                
             }
-
 
         // switch statement to set specific colours if value is specific thing ie condiment name?
             },
 
     render: function() {
         var displayIngredient = (ingredient) =>
-        <div id ={"ing"} className={'ingList'} onMouseOver={this.setColours}>
+        <div id ={ingredient} className={'ingList'} onMouseOver={this.setColours}>
             <li>
         {ingredient}
         </li>
