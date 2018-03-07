afd27b42e39673b417b296b5799f5bd06f50c3a2
Chelsey <jacobs27c@gmail.com>
2018-03-07 14:36:23 +0000

    Add switch statement to setColours to keep Bread, Mayo etc set colours
    One bug: Lettuce is Ketchup colour

--- makesandwich/ingredients.js
+++ makesandwich/ingredients.js
@@ -3,9 +3,26 @@ const Ingredients = React.createClass({
     setColours :   function(e) {
             var elementsList = document.getElementsByClassName('ingList');
             for (let i = 0; i < elementsList.length; i++) {
-                // randomising the unrecognised colours
-                elementsList[i].style.backgroundColor = '#'+Math.floor(Math.random() * 16777215).toString(16);
-                
+                // elementsList[i].style.backgroundColor = '#'+Math.floor(Math.random() * 16777215).toString(16);
+                switch(elementsList[i].innerText) {
+                    case "Bread":
+                        elementsList[i].style.backgroundColor = "#b08568";
+                        break;
+                    case "Lettuce":
+                        elementsList[i].style.backgroundColor = "#90EE90";
+                    case "Ketchup":
+                        elementsList[i].style.backgroundColor = "#E31100";
+                        break;
+                    case "Mustard":
+                        elementsList[i].style.backgroundColor = "#ffd500";
+                        break;
+                    case "Mayo":
+                        elementsList[i].style.backgroundColor = "whitesmoke";
+                        break;
+                    default:
+                        // randomising the unrecognised colours
+                        elementsList[i].style.backgroundColor = '#'+Math.floor(Math.random() * 16777215).toString(16);
+                }
             }
 
         // switch statement to set specific colours if value is specific thing ie condiment name?
