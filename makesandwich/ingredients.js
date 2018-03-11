3a93f85e09a221148ddff29a89e526b0de127036
Chelsey <jacobs27c@gmail.com>
2018-03-11 17:09:52 +0000

    Fix colour changing bugs

--- makesandwich/ingredients.js
+++ makesandwich/ingredients.js
@@ -1,49 +1,5 @@
 const Ingredients = React.createClass({
 
-    // UNSUCCESSFUL ATTEMPT AT SOLVING WEIRD CONDIMENT CHANGING COLOUR BUG
-
-    // setColours :   function(e) {
-    //     function setRandomColour (e) {
-    //     e.target.style.backgroundColor = '#'+Math.floor(Math.random() * 16777215).toString(16);
-    //     }
-    //         e.target.removeEventListener("onmouseenter", setRandomColour);
-    //         // var ingText = e.target.innerText;
-    //         var elementsList = document.getElementsByClassName('ingList');
-    //         for (let i = 0; i < elementsList.length; i++) {
-    //             var elementText = elementsList[i].innerText;
-    //             if (elementText.toUpperCase() !== "BREAD" | "LETTUCE" || "KETCHUP" || "MUSTARD" || "MAYO") {
-    //                     // this ensures that only the moused-over, non-condiment ingredient changes
-    //                     e.target.addEventListener("onmouseenter", setRandomColour(e, i));
-    //             }
-    //             else if (elementText.toUpperCase() === "BREAD" || "LETTUCE" || "KETCHUP" || "MUSTARD" || "MAYO") {
-    //                 switch(elementText.toUpperCase()) {
-    //                     // this switch ensures the condiments display correctly
-    //                 case "BREAD":
-    //                     elementsList[i].style.backgroundColor = "#b08568";
-    //                      elementsList[i].style.height = "45px";
-    //                      elementsList[i].style.lineHeight = "45px";
-    //                     break;
-    //                 case "LETTUCE":
-    //                     elementsList[i].style.backgroundColor = "#90EE90";
-    //                     break;
-    //                 case "KETCHUP":
-    //                     elementsList[i].style.backgroundColor = "#E31100";
-    //                     break;
-    //                 case "MUSTARD":
-    //                     elementsList[i].style.backgroundColor = "#ffd500";
-    //                     break;
-    //                 case "MAYO":
-    //                     elementsList[i].style.backgroundColor = "whitesmoke";
-    //                     break;
-    //                 default:
-    //                     break;
-    //                     // this ensures that only the moused-over ingredient changes
-    //                     e.target.addEventListener("onmouseenter", setRandomColour(e, i));
-    //             }
-    //             }
-    //         }
-    //         },
-
         setColours :   function(e) {
             var elementsList = document.getElementsByClassName('ingList');
             for (let i = 0; i < elementsList.length; i++) {
@@ -67,11 +23,22 @@ const Ingredients = React.createClass({
                         elementsList[i].style.backgroundColor = "whitesmoke";
                         break;
                     default:
-                        function setRandomColour (e) {
+                        if(e.target.tagName.toLowerCase() === 'li') {
+                            break;
+                        }
+                        else if (e.target.tagName.toLowerCase() === 'div' && e.target.innerText.toUpperCase() !== "BREAD"
+                        && e.target.innerText.toUpperCase() !== "LETTUCE"
+                        && e.target.innerText.toUpperCase() !== "KETCHUP"
+                        && e.target.innerText.toUpperCase() !== "MUSTARD"
+                        && e.target.innerText.toUpperCase() !== "MAYO") {
+                            function setRandomColour (e) {
                             e.target.style.backgroundColor = '#'+Math.floor(Math.random() * 16777215).toString(16);
                         }
-                        // this ensures that only the moused-over ingredient changes
-                        e.target.addEventListener("onmouseenter", setRandomColour(e, i));
+                            setRandomColour(e,i)
+                        }
+                        else {
+                            break;
+                        }
                 }
             }
             },
